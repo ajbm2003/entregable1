f4675e39499b60e2f0de83db788b8300634df8db
ajmb2003 <alejobenavides2003@gmail.com>
2023-10-16 13:49:55 -0300

    segundo commit

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -14,7 +14,7 @@ const Quote = ({quote, setQuote, setCurrentBg, backgrounds}) => {
             <article className='quoteCard__container'>
                 <h1 className='quoteCard__title'>Infogalax</h1>
                 <p className='quoteCard__phrase'>{quote.phrase}</p>
-                <button className='quoteCard__btn'onClick={handleChangeQuote}>Change</button>
+                <button className='quoteCard__btn' onClick={handleChangeQuote}>Change</button>
             </article>
             <footer className='quoteCard__footer'>
             Author:{quote.author}
